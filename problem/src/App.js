aa52248558b862601d044ede03b562d77f62dc3d
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2023-12-03 13:44:09 +0530

    React closure example added,

--- problem/src/App.js
+++ problem/src/App.js
@@ -1,5 +1,26 @@
-function App() {
-  return <div></div>;
-}
+import "./App.css";
+import { useState } from "react";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const onIncrease = () => {
+    setCount((prvCount) => prvCount + 1);
+  };
+
+  const onAlert = () => {
+    setTimeout(() => {
+      alert(`Count after 2s is : ${count}`);
+    }, 2000);
+  };
+
+  return (
+    <div className="container">
+      <p>Current count is : {count}</p>
+      <button onClick={onAlert}>Alert</button>
+      <button onClick={onIncrease}>Increase</button>
+    </div>
+  );
+};
 
 export default App;
--- solution/src/App.js
+++ solution/src/App.js
@@ -1,5 +1,29 @@
-function App() {
-  return <div></div>;
-}
+import "./App.css";
+import { useRef, useState } from "react";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const countRef = useRef(count);
+
+  const onIncrease = () => {
+    setCount((prvCount) => prvCount + 1);
+  };
+
+  countRef.current = count;
+
+  const onAlert = () => {
+    setTimeout(() => {
+      alert(`Count after 2s is : ${countRef.current}`);
+    }, 2000);
+  };
+
+  return (
+    <div className="container">
+      <p>Current count is : {count}</p>
+      <button onClick={onAlert}>Alert</button>
+      <button onClick={onIncrease}>Increase</button>
+    </div>
+  );
+};
 
 export default App;
